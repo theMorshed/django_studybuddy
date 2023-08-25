15d23e810c8362d6f548a997bdbabc6a5368de9a
theMorshed <themorshedctg@gmail.com>
2023-08-25 17:54:44 +0600

    get dynamic data without using database

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from base.views import home, room
+from base.views import home, room, single_room
 
 urlpatterns = [
     path('', home, name='home'),
-    path('room/', room, name='room'),
+    path('room/', room),
+    path('room/<str:pk>/', single_room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -8,7 +8,15 @@ rooms = [
     {'id': 4, 'name': 'Hello Universe'},
 ]
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
+
+def single_room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/single_room.html', context)
