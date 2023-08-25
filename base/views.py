d7067cab4bfb20816203172bbab3ddd6b2eebd45
theMorshed <themorshedctg@gmail.com>
2023-08-25 17:36:00 +0600

    send data in template with context

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Learn Python'},
+    {'id': 2, 'name': 'Javascript es6 learning'},
+    {'id': 3, 'name': 'Discuss with PHP'},
+    {'id': 4, 'name': 'Hello Universe'},
+]
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
