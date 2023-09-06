54df82876e23c72b11b5e613898bae7b313f7592
theMorshed <themorshedctg@gmail.com>
2023-09-07 00:08:17 +0600

    room update, room delete function implemented

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from base.views import home, room, single_room, create_room
+from base.views import home, room, single_room, create_room, edit_room, delete_room
 
 urlpatterns = [
     path('', home, name='home'),
     path('room/', room),
     path('room/<str:pk>/', single_room, name='room'),
     path('create_room/', create_room, name='create_room'),
+    path('edit_room/<int:pk>/', edit_room, name='edit_room'),
+    path('delete_room/<int:pk>/', delete_room, name='delete_room'),
 ]
--- base/views.py
+++ base/views.py
@@ -25,3 +25,21 @@ def create_room(request):
             
     context = {'form': form}
     return render(request, 'base/create_room.html', context)
+
+def edit_room(request, pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = RoomForm(instance = room)
+    context = {'form': form}
+    return render(request, 'base/edit_room.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete_room.html')
